3c99f55b3d3775ec098992f548aa060024cc5b64
durvasa <ferranmuinos@gmail.com>
2018-12-01 23:16:32 +0100

    AoC2018 Day 1 cleaner code

--- 2018/ferran/day01/ChronalCalibration.hs
+++ 2018/ferran/day01/ChronalCalibration.hs
@@ -10,13 +10,12 @@ parseLines = fmap lines . readFile
 -- coerce those strings to integers
 
 strToInt :: [Char] -> Int
-strToInt ('+':xs) = decInt xs
-strToInt ('-':xs) = negate $ decInt xs
+strToInt x = case x of
+             ('+':xs) -> decInt xs
+             ('-':xs) -> negate $ decInt xs
+   where decInt xs = foldl (\x y -> 10*x + y) 0 (map digitToInt xs)
 
-decInt :: [Char] -> Int
-decInt xs = foldl (\x y -> 10*x + y) 0 (map digitToInt xs)
-
--- do the sum
+-- do the sum in the IO monad
 
 toSum :: FilePath -> IO Int
 toSum fpath = liftM (sum . map strToInt) (parseLines fpath) 
@@ -28,38 +27,39 @@ toSum fpath = liftM (sum . map strToInt) (parseLines fpath)
 toList :: FilePath -> IO [Int]
 toList fpath = liftM (map strToInt) (parseLines fpath)
 
--- simple partial sum
+-- partial sums
 
 partialSum :: [Int] -> [Int]
-partialSum xs = scanl (+) 0 xs
+partialSum xs = tail $ scanl (+) 0 xs
 
--- if we cycle the list of frequencies, the partial sums will keep repeating modulo 525 (total sum)
--- we must find the pair of elements a < b in the list of partial sums at the least distance that 
--- is exactly a multiple of 525 (total sum)
+-- idea: if we cycle the list of frequencies, the partial sums will repeat modulo 525 (total sum);
+-- find the pair of elements in the list of partial sums with least distance that is exactly 
+-- a multiple of total sum
 
--- find all pairs in partial sums at distance multiple of total sum
+-- find all pairs at distance a multiple of n
 
 congru :: Int -> [Int] -> [(Int, Int)]
-congru n xs = congruList 
+congru n xs = map flip congruList 
     where
-      congruList = [(a, b) | (i, j) <- index, let a=xs!!(i-1), let b=xs!!(j-1), (a-b) `mod` n == 0, a <= b]
-      index = [(i, j) | (i, j) <- cartProd [1..length xs] [1..length xs], i < j]
-      cartProd = (<*>) . fmap (,) -- just the Cartesian product
+      congruList      = [(a, b) | (a, b) <- cart2Prod xs, (a-b) `mod` n == 0, a /= b]
+      cart2Prod xs    = [(snd x, snd y) | x <- zip [1..] xs, y <- zip [1..] xs, fst x /= fst y]
+      flip (x, y)
+          | x <= y    = (x, y)
+          | otherwise = (y, x) 
 
--- find those that have least distance modulo the total sum
--- we take care to remove the trivial cases
+-- now find those at least distance
 
-minModDist :: [(Int, Int)] -> Int
-minModDist xs = get2nd headElem
-    where headElem = head $ removeTrivial $ sortBy (\(_,_,a) (_,_,b) -> compare a b) (map addDiff xs)
-          addDiff (x, y) = (x, y, y - x)
-          removeTrivial ys = filter (\(a,b,c) -> (a /= 0) || (b == 0)) ys
-          get2nd (_,z,_) = z
+firstRepeat :: Int -> [Int] -> (Int,Int,Int)
+firstRepeat n xs = head sorted
+    where pairList = congru n (partialSum xs) 
+          sorted   = sortBy (\(_,_,a) (_,_,b) -> compare a b) (map addDiff pairList)
+          addDiff (x, y) = (x, y, abs (x - y))   
 
--- do the job in the IO monad
+-- finish the job in the IO monad
 
 firstRepeatIO :: FilePath -> IO Int
-firstRepeatIO fpath = liftM minModDist (liftM (congru 525) (liftM partialSum (toList fpath)))
+firstRepeatIO fpath = liftM (get2nd . firstRepeat 525) (toList fpath)
+    where get2nd (_, a, _) = a
 
 -- second part
 -- firstRepeatIO "input.txt"
